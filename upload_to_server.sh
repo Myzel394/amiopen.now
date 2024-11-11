ce52341ad8aa984acb20a985f7bbb0dd5a64305e
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-11-11 22:33:46 +0100

    refactor: Move server into its own folder

--- upload_to_server.sh
+++ upload_to_server.sh
@@ -1,9 +1,9 @@
 ip=$1
 user=$2
 
-docker buildx build --platform linux/amd64 . --tag amiopen:latest
-docker save -o amiopen.tar amiopen:latest
-
-scp amiopen.tar $user@$ip:~/amiopen/amiopen.tar
-ssh $user@$ip "cd ~/amiopen && docker container stop amiopen && docker container rm amiopen && docker load -i amiopen.tar && docker compose up -d"
+cd server && 
+    docker buildx build --platform linux/amd64 . --tag amiopen:latest && 
+    docker save -o amiopen.tar amiopen:latest && 
+    scp amiopen.tar $user@$ip:~/amiopen/amiopen.tar &&
+    ssh $user@$ip "cd ~/amiopen && docker container stop amiopen && docker container rm amiopen && docker load -i amiopen.tar && docker compose up -d"
 
